f2f897e667db05f2fcace7050dfbb9877472dbf4
Jack Phoenix <ashley@users.mediawiki.org>
2011-03-30 19:19:04 +0000

    add LinkSuggest extension; tested with FF 3.6.13/15 and IE7 + 8 (IIRC), has some known issues on IE9. Huge thanks to Jesús Martínez Novo for rewriting the main JS file!

---
+++ LinkSuggest.i18n.php
@@ -0,0 +1,305 @@
+<?php
+/**
+ * Internationalization file for the LinkSuggest extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English */
+$messages['en'] = array(
+	'tog-disablelinksuggest' => 'Do not show link suggest'
+);
+
+/** Afrikaans (Afrikaans)
+ * @author Naudefj
+ */
+$messages['af'] = array(
+	'tog-disablelinksuggest' => 'Moet nie skakel-voorstelle wys nie',
+);
+
+/** Azerbaijani (Azərbaycanca)
+ * @author PPerviz
+ */
+$messages['az'] = array(
+	'tog-disablelinksuggest' => 'Keçid təklifini göstərmə',
+);
+
+/** Belarusian (Taraškievica orthography) (‪Беларуская (тарашкевіца)‬)
+ * @author EugeneZelenko
+ */
+$messages['be-tarask'] = array(
+	'tog-disablelinksuggest' => 'Не паказваць прапановы спасылак',
+);
+
+/** Breton (Brezhoneg)
+ * @author Gwenn-Ael
+ */
+$messages['br'] = array(
+	'tog-disablelinksuggest' => 'Chomp hep diskouez al liammoù kinniget',
+);
+
+/** Catalan (Català)
+ * @author Paucabot
+ */
+$messages['ca'] = array(
+	'tog-disablelinksuggest' => "No mostris suggeriments d'enllaços",
+);
+
+/** Czech (Česky)
+ * @author Mormegil
+ */
+$messages['cs'] = array(
+	'tog-disablelinksuggest' => 'Nezobrazovat návrhy odkazů',
+);
+
+/** German (Deutsch) */
+$messages['de'] = array(
+	'tog-disablelinksuggest' => 'Keine Link-Vorschläge anzeigen',
+);
+
+/** Spanish (Español) */
+$messages['es'] = array(
+	'tog-disablelinksuggest' => 'No mostrar sugerencias de vínculos',
+);
+
+/** Finnish (Suomi)
+ * @author Centerlink
+ */
+$messages['fi'] = array(
+	'tog-disablelinksuggest' => 'Älä näytä linkkiehdotuksia',
+);
+
+/** French (Français) */
+$messages['fr'] = array(
+	'tog-disablelinksuggest' => 'Ne pas montrer les liens suggérés',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'tog-disablelinksuggest' => 'Non mostrar as ligazón suxeridas',
+);
+
+/** Manx (Gaelg)
+ * @author MacTire02
+ */
+$messages['gv'] = array(
+	'tog-disablelinksuggest' => 'Ny taishbyney kiangley-choyrlaghey',
+);
+
+/** Hebrew (עברית)
+ * @author Rotemliss
+ */
+$messages['he'] = array(
+	'tog-disablelinksuggest' => 'ביטול הצגת הצעות לקישורים',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'tog-disablelinksuggest' => 'Žadyn wotkazowy namjet pokazać',
+);
+
+/** Hungarian (Magyar)
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'tog-disablelinksuggest' => 'Ne jelenítse meg a hivatkozás-javaslatot',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'tog-disablelinksuggest' => 'Non monstrar suggestiones de ligamines',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author Bennylin
+ */
+$messages['id'] = array(
+	'tog-disablelinksuggest' => 'Jangan menampilkan saran pranala',
+);
+
+/** Italian (Italiano)
+ * @author Civvì
+ */
+$messages['it'] = array(
+	'tog-disablelinksuggest' => 'Non mostrare link suggeriti',
+);
+
+/** Japanese (日本語) */
+$messages['ja'] = array(
+	'tog-disablelinksuggest' => '編集時のリンク補完を使わない',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'tog-disablelinksuggest' => 'Donn kein Vörschlääsch för Lenks aanzeije',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'tog-disablelinksuggest' => 'Déi proposéiert Linken net weisen',
+);
+
+/** Latvian (Latviešu)
+ * @author Xil
+ */
+$messages['lv'] = array(
+	'tog-disablelinksuggest' => 'Neradīt saišu ieteikumus',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'tog-disablelinksuggest' => 'Не прикажувај предложени врски',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'tog-disablelinksuggest' => 'Geen verwijzigssuggesties weergeven',
+);
+
+/** Norwegian Nynorsk (‪Norsk (nynorsk)‬)
+ * @author Harald Khan
+ */
+$messages['nn'] = array(
+	'tog-disablelinksuggest' => 'Ikkje vis lenkjeframlegg',
+);
+
+/** Norwegian (bokmål)‬ (‪Norsk (bokmål)‬)
+ * @author Jon Harald Søby
+ */
+$messages['no'] = array(
+	'tog-disablelinksuggest' => 'Ikke vis lenkeforslag',
+);
+
+/** Occitan (Occitan)
+ * @author Cedric31
+ */
+$messages['oc'] = array(
+	'tog-disablelinksuggest' => 'Far pas veire los ligams suggerits',
+);
+
+/** Polish (Polski)
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'tog-disablelinksuggest' => 'Nie pokazuj sugerowanych linków',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ */
+$messages['pms'] = array(
+	'tog-disablelinksuggest' => "Mostra pa ij sugeriment ëd j'anliure",
+);
+
+/** Pashto (پښتو)
+ * @author Ahmed-Najib-Biabani-Ibrahimkhel
+ */
+$messages['ps'] = array(
+	'tog-disablelinksuggest' => 'د تړنې وړانديز مه ښکاره کوه',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'tog-disablelinksuggest' => 'Não apresentar sugestões de links',
+);
+
+/** Brazilian Portuguese (Português do Brasil)
+ * @author Daemorris
+ */
+$messages['pt-br'] = array(
+	'tog-disablelinksuggest' => 'Não mostrar sugestões de ligação',
+);
+
+/** Tarandíne (Tarandíne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'tog-disablelinksuggest' => "No fà vedè 'u collegamende suggerite",
+);
+
+/** Russian (Русский) */
+$messages['ru'] = array(
+	'tog-disablelinksuggest' => 'Не показывать предложения ссылок',
+);
+
+/** Serbian Cyrillic ekavian (‪Српски (ћирилица)‬)
+ * @author Rancher
+ * @author Verlor
+ */
+$messages['sr-ec'] = array(
+	'tog-disablelinksuggest' => 'Не приказуј предложену везу',
+);
+
+/** Sundanese (Basa Sunda)
+ * @author Kandar
+ */
+$messages['su'] = array(
+	'tog-disablelinksuggest' => 'Sumputkeun usulan tutumbu',
+);
+
+/** Swedish (Svenska)
+ * @author Sertion
+ */
+$messages['sv'] = array(
+	'tog-disablelinksuggest' => 'Visa inte länkförslag',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'tog-disablelinksuggest' => 'Huwag ipakita ang mungkahing kawing',
+);
+
+/** Turkish (Türkçe)
+ * @author Manco Capac
+ */
+$messages['tr'] = array(
+	'tog-disablelinksuggest' => 'Bağlantı önermesini gösterme',
+);
+
+/** Tatar (Cyrillic) (Татарча/Tatarça (Cyrillic))
+ * @author Ильнар
+ */
+$messages['tt-cyrl'] = array(
+	'tog-disablelinksuggest' => 'Сылтамаларны күрсәтмәскә',
+);
+
+/** Ukrainian (Українська) */
+$messages['uk'] = array(
+	'tog-disablelinksuggest' => 'Не показувати пропозиції посилань',
+);
+
+/** Simplified Chinese (‪中文(简体)‬)
+ * @author Hydra
+ */
+$messages['zh-hans'] = array(
+	'tog-disablelinksuggest' => '不会显示链接建议',
+);
+
+/** Traditional Chinese (‪中文(繁體)‬)
+ * @author Frankou
+ */
+$messages['zh-hant'] = array(
+	'tog-disablelinksuggest' => '不顯示鏈接建議',
+);
+
