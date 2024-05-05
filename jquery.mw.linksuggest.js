045724f5db9a42ed2933415eed9932339f2fcc6e
Jayden Bailey <jayden@weirdgloop.org>
2024-05-05 21:46:21 +0100

    Refactor to use the core prefixsearch API module
    LinkSuggest has maintained its own API for fetching suggestions for a long while, which has become more and more unnecessary in recent years. MediaWiki 1.23 introduced the prefixsearch API module, which is more than sufficient for providing suggestions for links, and supports resolving redirect targets. That same core API module is already used extensively in MediaWiki across both WikiEditor and VisualEditor as part of the "Insert link" features that those extensions have.
    
    This is a breaking change for any external consumers of the linksuggest API module, since this change removes that module entirely. For MediaWiki consumers that do not manually make use of the linksuggest API (e.g those who just install the extension and go - which are likely the majority of people), there should be no negative behaviour from this change.
    
    Bug: T273898
    Change-Id: I29035c2253e033122abe39405942d260e251e619

--- jquery.mw.linksuggest.js
+++ jquery.mw.linksuggest.js
@@ -1,10 +1,11 @@
 /*
- * jQuery MediaWiki LinkSuggest 1.9
+ * jQuery MediaWiki LinkSuggest 2.2
  * JavaScript for LinkSuggest extension
  *
  * Copyright © 2010-2015
  * Authors: Inez Korczyński (korczynski at gmail dot com)
  *          Jesús Martínez Novo (martineznovo at gmail dot com)
+ *          Jayden Bailey (jayden@weirdgloop.org)
  * Licensed under the GPL (GPL-LICENSE.txt) license.
  *
  * Depends:
@@ -19,7 +20,8 @@
 		cachedElement = null, // Cached element of the autocomplete to compare
 		cachedText = null, // Cached text before the position of the link
 		cachedPosition = null, // Cached position of the drop-down
-		suppressKeyPress, suppressInput, suppressKeyPressRepeat;
+		config = require( './config.json' ),
+		suppressKeyPress, suppressKeyPressRepeat;
 
 	$.widget( 'mw.linksuggest', {
 		options: {
@@ -76,13 +78,11 @@
 			var keyCode;
 			if ( this.element.prop( 'readOnly' ) ) {
 				suppressKeyPress = true;
-				suppressInput = true;
 				suppressKeyPressRepeat = true;
 				return;
 			}
 
 			suppressKeyPress = false;
-			suppressInput = false;
 			suppressKeyPressRepeat = false;
 			keyCode = $.ui.keyCode;
 			switch ( event.keyCode ) {
@@ -189,24 +189,24 @@
 				c = text.charAt( i );
 				c1 = ( i > 0 ? text.charAt( i - 1 ) : '' );
 				// A line break, it isn't closed
-				if ( c == '\n' ) {
+				if ( c === '\n' ) {
 					break;
 				}
 				// A start of a link, so this link isn't closed
-				if ( c == '[' && c1 == '[' ) {
+				if ( c === '[' && c1 === '[' ) {
 					break;
 				}
 				// A closing link and this was a link, exit
-				if ( c == ']' && c1 == ']' ) {
+				if ( c === ']' && c1 === ']' ) {
 					response( emptyset );
 					return false;
 				}
 				// A start of a template, so this template isn't closed
-				if ( c == '{' && c1 == '{' ) {
+				if ( c === '{' && c1 === '{' ) {
 					break;
 				}
 				// A closing template and this was a template, exit
-				if ( c == '}' && c1 == '}' ) {
+				if ( c === '}' && c1 === '}' ) {
 					response( emptyset );
 					return false;
 				}
@@ -216,21 +216,21 @@
 			for ( i = caret - 1; i >= 0; i-- ) {
 				c = text.charAt( i );
 				// If nothing found after a line break, nothing to match
-				if ( c == '\n' ) {
+				if ( c === '\n' ) {
 					break;
 				}
 				// Closed link/template, a pipe or a hash.
 				// There's no link/template to complete, or we're on a parser
 				// function or link hash
-				if ( c == ']' || c == '}' || c == '|' || c == '#' ) {
+				if ( c === ']' || c === '}' || c === '|' || c === '#' ) {
 					response( emptyset );
 					return false;
 				}
 
 				// It's an open link
-				if ( c == '[' && i > 0 && text.charAt( i - 1 ) == '[' ) {
+				if ( c === '[' && i > 0 && text.charAt( i - 1 ) === '[' ) {
 					sQueryReal = text.substr( i + 1, ( caret - i - 1 ) );
-					if ( sQueryReal.charAt( 0 ) == ':' ) {
+					if ( sQueryReal.charAt( 0 ) === ':' ) {
 						sQueryReal = sQueryReal.slice( 1 );
 						format = '[[:$1]]';
 					} else {
@@ -241,15 +241,15 @@
 				}
 
 				// It's an open template
-				if ( c == '{' && i > 0 && text.charAt( i - 1 ) == '{' ) {
+				if ( c === '{' && i > 0 && text.charAt( i - 1 ) === '{' ) {
 				// Exclude template parameters
-					if ( i > 1 && text.charAt( i - 2 ) == '{' ) {
+					if ( i > 1 && text.charAt( i - 2 ) === '{' ) {
 						response( emptyset );
 						return false;
 					}
 					sQueryReal = text.substr( i + 1, ( caret - i - 1 ) );
-					if ( sQueryReal.length >= 6 && sQueryReal.toLowerCase().slice( 0, 6 ) == 'subst:' ) {
-						if ( sQueryReal.length >= 7 && sQueryReal.charAt( 6 ) == ':' ) {
+					if ( sQueryReal.length >= 6 && sQueryReal.toLowerCase().slice( 0, 6 ) === 'subst:' ) {
+						if ( sQueryReal.length >= 7 && sQueryReal.charAt( 6 ) === ':' ) {
 							sQueryReal = sQueryReal.slice( 7 );
 							format = '{{subst::$1}}';
 						} else {
@@ -257,7 +257,7 @@
 							stripPrefix = true;
 							format = '{{subst:$1}}';
 						}
-					} else if ( sQueryReal.charAt( 0 ) == ':' ) {
+					} else if ( sQueryReal.charAt( 0 ) === ':' ) {
 						sQueryReal = sQueryReal.slice( 1 );
 						format = '{{:$1}}';
 					} else {
@@ -273,9 +273,12 @@
 			if ( sQueryStartAt >= 0 && sQueryReal.length >= this.options.minLength ) {
 				api = new mw.Api();
 				api.get( {
-					action: 'linksuggest',
-					get: 'suggestions',
-					query: sQueryReal
+					action: 'query',
+					generator: 'prefixsearch',
+					gpsnamespace: config.LinkSuggestFromNamespaces,
+					gpssearch: sQueryReal,
+					gpslimit: 10,
+					redirects: true
 				} ).done( this._responseWrapper( this, response, format, stripPrefix ) );
 				return true;
 			}
@@ -284,17 +287,48 @@
 		},
 		_responseWrapper: function ( thisArg, callback, format, stripPrefix ) {
 			return function ( data ) {
-				if ( !data || data.error ) {
+				if ( !data || !data.query || data.error ) {
 					return callback( [] );
 				}
-				callback( thisArg._formatResponse( data, format, stripPrefix ) );
+
+				// The rest of this function is quite similar to mw.widgets.TitleWidget.prototype.getOptionsFromData
+				var i, len, index, suggestionPage, redirect, redirects,
+					titles = [],
+					redirectsTo = {},
+					pageIndexes = {};
+
+				if ( data.query.redirects ) {
+					for ( i = 0, len = data.query.redirects.length; i < len; i++ ) {
+						redirect = data.query.redirects[ i ];
+						redirectsTo[ redirect.to ] = redirectsTo[ redirect.to ] || [];
+						redirectsTo[ redirect.to ].push( redirect.from );
+					}
+				}
+
+				for ( index in data.query.pages ) {
+					suggestionPage = data.query.pages[ index ];
+					pageIndexes[ suggestionPage.title ] = suggestionPage.index;
+					titles.push( suggestionPage.title );
+
+					redirects = Object.prototype.hasOwnProperty.call( redirectsTo, suggestionPage.title ) ? redirectsTo[ suggestionPage.title ] : [];
+					for ( i = 0, len = redirects.length; i < len; i++ ) {
+						pageIndexes[ redirects[ i ] ] = suggestionPage.index + 0.5;
+						titles.push( redirects[ i ] );
+					}
+				}
+
+				titles.sort( function ( a, b ) {
+					return pageIndexes[ a ] - pageIndexes[ b ];
+				} );
+
+				callback( thisArg._formatResponse( titles, format, stripPrefix ) );
 			};
 		},
 		_formatResponse: function ( data, format, stripPrefix ) {
-			return $.map( data.linksuggest.result.suggestions, function ( n ) {
+			return $.map( data, function ( n ) {
 				if ( stripPrefix ) {
 					var pos = n.indexOf( ':' );
-					if ( pos != -1 ) {
+					if ( pos !== -1 ) {
 						n = n.slice( pos + 1 );
 					}
 				}
@@ -310,7 +344,7 @@
 				prefix = oItem.value.slice( 0, 2 );
 
 			for ( var i = caret - 2; i >= 0; i-- ) { // break for templates and normal links
-				if ( text.substr( i, 2 ) == prefix ) {
+				if ( text.substr( i, 2 ) === prefix ) {
 					break;
 				}
 			}
@@ -337,7 +371,7 @@
 					caretPos++;
 				}
 				// Firefox support
-			} else if ( control.selectionStart || control.selectionStart == '0' ) {
+			} else if ( control.selectionStart || control.selectionStart === '0' ) {
 				caretPos = control.selectionStart;
 			}
 			return caretPos;
@@ -378,7 +412,7 @@
 			// Get the position at the start of the link/template
 			for ( i = caret - 1; i >= 0; i-- ) {
 				c = text.charAt( i );
-				if ( c == '[' || c == '{' ) {
+				if ( c === '[' || c === '{' ) {
 					initialCaret = i + 1;
 					break;
 				}
@@ -425,7 +459,7 @@
 			cachedPosition = result;
 			return result;
 		},
-		_open: function ( event, ui ) {
+		_open: function () {
 			var menu = this.element.data( 'autocomplete' ).menu.element,
 				offset = this._getCaretPosition(),
 				width = menu.outerWidth(),
